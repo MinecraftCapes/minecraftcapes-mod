dfb0b0566070eb9dd2570e030ee9d2ee3f4694a6
James Harrison <james@fasttortoise.co.uk>
2019-01-08 14:33:40 +0000

    Initial Commit

---
+++ src/main/java/co/uk/minecraftcapes/Reference.java
@@ -0,0 +1,16 @@
+package co.uk.minecraftcapes;
+
+public class Reference {
+
+	public static final String MOD_ID = "jmcm";
+	//Name of Mod
+	public static final String NAME = "James090500's MinecraftCapes Mod";
+	//Version of mod
+	public static final String VERSION = "9.1";
+	//MC Version running for
+	public static final String MC_VERSION = "[1.10,1.11]";
+	
+	public static final String CLIENT_PROXY_CLASS = "co.uk.minecraftcapes.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "co.uk.minecraftcapes.proxy.ServerProxy";
+	
+}
