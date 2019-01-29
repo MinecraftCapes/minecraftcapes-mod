79e210bc9676b0b9e86810513b9b99037eaf4ec4
Siriuo <siriuo@outlook.com>
2019-01-29 12:52:07 +0000

    Update forge version to build 114

--- src/main/java/co/uk/minecraftcapes/player/downloader/DownloadCape.java
+++ src/main/java/co/uk/minecraftcapes/player/downloader/DownloadCape.java
@@ -16,7 +16,7 @@ public class DownloadCape {
 		
 	    if ((uuid != null) && (!uuid.isEmpty())) {
 
-	    	String url = "https://www.MinecraftCapes.co.uk/getCape.php?uuid=" + uuid;	    	
+	    	String url = "https://www.MinecraftCapes.co.uk/getCape.php?uuid=" + uuid;
 	    	
 		    ResourceLocation rl = new ResourceLocation(MODID, "capes/" + uuid);
 		    TextureManager textureManager = Minecraft.getInstance().getTextureManager();
