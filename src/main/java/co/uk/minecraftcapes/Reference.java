7af8c0078cc70c40c10f54baf8b253215a39206f
James Harrison <james@fasttortoise.co.uk>
2019-10-02 20:52:01 +0100

    Fixed versions

--- src/main/java/co/uk/minecraftcapes/Reference.java
+++ src/main/java/co/uk/minecraftcapes/Reference.java
@@ -8,7 +8,7 @@ public class Reference {
 	//Version of mod
 	public static final String VERSION = "9.5";
 	//MC Version running for
-	public static final String MC_VERSION = "[1.8.9,1.8.9]";
+	public static final String MC_VERSION = "[1.8.8,1.8.9]";
 	
 	public static final String CLIENT_PROXY_CLASS = "co.uk.minecraftcapes.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "co.uk.minecraftcapes.proxy.ServerProxy";
