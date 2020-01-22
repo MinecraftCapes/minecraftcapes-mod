e207c4935ab926dfb002b27e0203716690b29395
Siriuo <siriuo@outlook.com>
2020-01-22 18:39:20 +0000

    Forgot to remove the test URL. Oh also updated forge :) JAMES PUSH THIS AS A DOWNLOAD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/main/java/co/uk/minecraftcapes/player/downloader/DownloadCape.java
+++ src/main/java/co/uk/minecraftcapes/player/downloader/DownloadCape.java
@@ -16,8 +16,8 @@ public class DownloadCape {
 		
 	    if ((uuid != null) && (!uuid.isEmpty())) {
 
-	    	//String url = "https://minecraftcapes.co.uk/getCape/" + uuid;
-			String url = "https://minecraftcapes.co.uk/getCape/061b485d1efa4b2fa2429cc59ce6370f";
+	    	String url = "https://minecraftcapes.co.uk/getCape/" + uuid;
+
 	    	
 		    ResourceLocation rl = new ResourceLocation(MODID, "capes/" + uuid);
 		    TextureManager textureManager = Minecraft.getInstance().getTextureManager();
