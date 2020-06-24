5eafe303c315129b6ecfe80c33292790aab54117
James Harrison <james@fasttortoise.co.uk>
2020-06-24 16:48:37 +0100

    Moved to .net

--- src/main/java/net/minecraftcapes/events/PlayerEventHandler.java
+++ src/main/java/net/minecraftcapes/events/PlayerEventHandler.java
@@ -21,8 +21,7 @@ public class PlayerEventHandler {
 
 		Thread playerDownload = new Thread(() -> {
 			try {
-				//Todo needs changing to .net
-				URL url = new URL("https://minecraftcapes.co.uk/profile/" + playerHandler.getPlayerUUID().toString().replace("-", ""));
+				URL url = new URL("https://minecraftcapes.net/profile/" + playerHandler.getPlayerUUID().toString().replace("-", ""));
 				HttpURLConnection httpurlconnection = (HttpURLConnection) url.openConnection(MinecraftClient.getInstance().getNetworkProxy());
 				httpurlconnection.setDoInput(true);
 				httpurlconnection.setDoOutput(false);
