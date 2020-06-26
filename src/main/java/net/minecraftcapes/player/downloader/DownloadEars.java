3479064ef285c807e17672305b939fef0379056f
James Harrison <james@fasttortoise.co.uk>
2020-06-26 09:48:04 +0100

    Update to v10

---
+++ src/main/java/net/minecraftcapes/player/downloader/DownloadEars.java
@@ -0,0 +1,40 @@
+package net.minecraftcapes.player.downloader;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftcapes.helpers.Downloader;
+import net.minecraftcapes.helpers.IImageBuffer;
+import net.minecraftcapes.player.PlayerHandler;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.UUID;
+
+import static net.minecraftcapes.MinecraftCapes.MODID;
+
+public class DownloadEars {
+
+	public static void download(String earsUrl, PlayerHandler playerHandler) {
+		UUID playerUUID = playerHandler.getPlayerUUID();
+		if(playerUUID != null) {
+	    	ResourceLocation rl = new ResourceLocation(MODID, "ears/" + playerUUID);
+	    	TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
+	    	Downloader textureEars = new Downloader(earsUrl, playerHandler.getEarLocation(), iImageBuffer, playerHandler);
+	    	textureManager.loadTexture(rl, textureEars);
+		}
+	}
+
+	private static final IImageBuffer iImageBuffer = new IImageBuffer() {
+		@Override
+		public BufferedImage parseTexture(BufferedImage img, PlayerHandler playerHandler) {
+			BufferedImage imgNew = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
+			Graphics g = imgNew.getGraphics();
+			g.drawImage(img, 24, 0, null);
+			g.dispose();
+			playerHandler.setHasEars(true);
+
+			return imgNew;
+		}
+	};
+}
