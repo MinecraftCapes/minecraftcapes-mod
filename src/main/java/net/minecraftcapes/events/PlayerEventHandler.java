6357e55d35b4e06be3d36b9fd201b43d8406a87a
James Harrison <james@fasttortoise.co.uk>
2020-07-05 13:04:25 +0100

    Added/Fixed Glint
    Renamed mod
    
    Fixed typo in Siriuo
    
    Changed build.gradle

--- src/main/java/net/minecraftcapes/events/PlayerEventHandler.java
+++ src/main/java/net/minecraftcapes/events/PlayerEventHandler.java
@@ -36,9 +36,6 @@ public class PlayerEventHandler {
 						ProfileResult profileResult = new Gson().fromJson(reader, ProfileResult.class);
 
 						playerHandler.setHasInfo(true);
-						playerHandler.setUpsideDown(profileResult.upsideDown);
-
-						playerHandler.setHasInfo(true);
 						playerHandler.setHasCapeGlint(profileResult.capeGlint);
 						playerHandler.setUpsideDown(profileResult.upsideDown);
 
