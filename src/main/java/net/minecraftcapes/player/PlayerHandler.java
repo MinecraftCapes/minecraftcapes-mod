4aabdea1533c950443dc0bc7b0c6c5efa32ecf78
James Harrison <james@fasttortoise.co.uk>
2020-07-05 13:04:11 +0100

    Added/Fixed Glint
    Renamed mod
    
    Fixed typo in Siriuo
    
    Changed build.gradle

--- src/main/java/net/minecraftcapes/player/PlayerHandler.java
+++ src/main/java/net/minecraftcapes/player/PlayerHandler.java
@@ -1,6 +1,5 @@
 package net.minecraftcapes.player;
 
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import lombok.Getter;
 import lombok.Setter;
 import net.minecraft.client.Minecraft;
@@ -8,8 +7,13 @@ import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftcapes.MinecraftCapes;
+import org.apache.commons.codec.binary.Base64;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -22,12 +26,13 @@ public class PlayerHandler {
     @Setter private boolean hasStaticCape = false;
     @Setter private boolean hasEars = false;
     @Setter private boolean hasAnimatedCape = false;
+    @Getter @Setter private Boolean hasCapeGlint = false;
     @Getter @Setter private boolean upsideDown = false;
     @Getter @Setter private Boolean hasInfo = false;
     @Setter @Getter private UUID playerUUID;
 
     @Getter
-    private Int2ObjectMap<BufferedImage> animatedCape;
+    private HashMap<Integer, BufferedImage> animatedCape;
 
     //Animated Cape Settings
     private long lastFrameTime = 0;
@@ -50,10 +55,82 @@ public class PlayerHandler {
     }
 
     /**
+     * Reads a base64 string and converts it to a BufferedImage
+     * @param textureBase64
+     * @return
+     */
+    private BufferedImage readTexture(String textureBase64) {
+        try {
+            byte[] imgBytes = Base64.decodeBase64(textureBase64);
+            ByteArrayInputStream bias = new ByteArrayInputStream(imgBytes);
+            return ImageIO.read(bias);
+        } catch (IOException e) {
+            MinecraftCapes.getLogger().error(e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Gets the cape texture and resizes or splits it accordingly
+     * @param cape
+     */
+    public void applyCape(String cape) {
+        BufferedImage capeImage = readTexture(cape);
+        //If the height is not 1/2 the width (32 == 64/2) then its an animated cape
+        if(capeImage.getHeight() != capeImage.getWidth() / 2) {
+            HashMap<Integer, BufferedImage> animatedCape = new HashMap<Integer, BufferedImage>();
+            int totalFrames = capeImage.getHeight() / (capeImage.getWidth() / 2);
+            for(int currentFrame = 0; currentFrame < totalFrames; currentFrame++) {
+                BufferedImage frame = new BufferedImage(capeImage.getWidth(), capeImage.getWidth() / 2, BufferedImage.TYPE_INT_ARGB);
+                Graphics frameGraphics = frame.getGraphics();
+                frameGraphics.drawImage(capeImage,
+                        0,
+                        0,
+                        capeImage.getWidth(),
+                        (capeImage.getWidth() / 2),
+                        0,
+                        (currentFrame * (capeImage.getWidth() / 2)),
+                        capeImage.getWidth(),
+                        ((currentFrame + 1) * (capeImage.getWidth() / 2)),
+                        null);
+                frameGraphics.dispose();
+                animatedCape.put(currentFrame, frame);
+            }
+            setAnimatedCape(animatedCape);
+            MinecraftCapes.getLogger().debug("Animated cape loaded for {}", playerUUID);
+        } else {
+            int imageWidth = 64;
+            int imageHeight = 32;
+
+            for (int srcWidth = capeImage.getWidth(), srcHeight = capeImage.getHeight(); imageWidth < srcWidth || imageHeight < srcHeight; imageWidth *= 2, imageHeight *= 2) {}
+
+            final BufferedImage imgNew = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB);
+            Graphics g = imgNew.getGraphics();
+            g.drawImage(capeImage, 0, 0, null);
+            g.dispose();
+
+            applyTexture(new ResourceLocation(MODID, "capes/" + playerUUID), imgNew);
+            setHasStaticCape(true);
+            MinecraftCapes.getLogger().debug("Static cape loaded for {}", playerUUID);
+        }
+    }
+
+    public void applyEars(String ears) {
+        BufferedImage earImage = readTexture(ears);
+        BufferedImage imgNew = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
+        Graphics g = imgNew.getGraphics();
+        g.drawImage(earImage, 24, 0, null);
+        g.dispose();
+        applyTexture(new ResourceLocation(MODID, "ears/" + playerUUID), imgNew);
+        this.setHasEars(true);
+    }
+
+    /**
      * Sets the animated cape textures and loads all resources to memory
      * @param animatedCape
      */
-    public void setAnimatedCape(Int2ObjectMap<BufferedImage> animatedCape) {
+    public void setAnimatedCape(HashMap<Integer, BufferedImage> animatedCape) {
         MinecraftCapes.getLogger().debug("Setting animated cape for {}", playerUUID);
         this.animatedCape = animatedCape;
         this.setHasAnimatedCape(true);
@@ -65,14 +142,9 @@ public class PlayerHandler {
      */
     private void loadFramesToResource() {
         MinecraftCapes.getLogger().debug("Loading resources to memory for {}", playerUUID);
-        for(final Int2ObjectMap.Entry<BufferedImage> entry : getAnimatedCape().int2ObjectEntrySet()) {
-            final ResourceLocation currentResource = new ResourceLocation(MODID, String.format("capes/%s/%d", playerUUID, entry.getIntKey()));
-            Minecraft.getMinecraft().addScheduledTask(new Runnable() {
-                @Override
-                public void run() {
-                    Minecraft.getMinecraft().getTextureManager().loadTexture(currentResource, new DynamicTexture(entry.getValue()));
-                }
-            });
+        for(final HashMap.Entry<Integer, BufferedImage> entry : getAnimatedCape().entrySet()) {
+            ResourceLocation currentResource = new ResourceLocation(MODID, String.format("capes/%s/%d", playerUUID, entry.getKey()));
+            applyTexture(currentResource, entry.getValue());
         }
     }
 
@@ -117,6 +189,20 @@ public class PlayerHandler {
     }
 
     /**
+     * Applys a texture on the render thread
+     * @param resourceLocation
+     * @param bufferedImage
+     */
+    private void applyTexture(final ResourceLocation resourceLocation, final BufferedImage bufferedImage) {
+        Minecraft.getMinecraft().addScheduledTask(new Runnable() {
+            @Override
+            public void run() {
+                Minecraft.getMinecraft().getTextureManager().loadTexture(resourceLocation, new DynamicTexture(bufferedImage));
+            }
+        });
+    }
+
+    /**
      * A nice to string thing
      * @return
      */
@@ -126,6 +212,8 @@ public class PlayerHandler {
                 "hasStaticCape=" + hasStaticCape +
                 ", hasEars=" + hasEars +
                 ", hasAnimatedCape=" + hasAnimatedCape +
+                ", hasCapeGlint=" + hasCapeGlint +
+                ", upsideDown=" + upsideDown +
                 ", hasInfo=" + hasInfo +
                 ", playerUUID=" + playerUUID +
                 ", animatedCape=" + animatedCape +
--- src/main/java/net/minecraftcapes/player/render/CapeLayer.java
+++ src/main/java/net/minecraftcapes/player/render/CapeLayer.java
@@ -3,18 +3,20 @@ package net.minecraftcapes.player.render;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.client.renderer.entity.layers.LayerArmorBase;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.entity.player.EnumPlayerModelParts;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftcapes.player.PlayerHandler;
+import net.minecraftcapes.player.model.ModelCape;
 
 public class CapeLayer implements LayerRenderer<AbstractClientPlayer> {
 
    private final RenderPlayer playerRenderer;
+   private final ModelCape modelCape = new ModelCape();
 
    public CapeLayer(RenderPlayer playerRendererIn) {
       this.playerRenderer = playerRendererIn;
@@ -23,12 +25,11 @@ public class CapeLayer implements LayerRenderer<AbstractClientPlayer> {
    @Override
    public void doRenderLayer(AbstractClientPlayer entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
       PlayerHandler playerHandler = PlayerHandler.getFromPlayer(entitylivingbaseIn);
-      ResourceLocation rl = playerHandler.getCapeLocation();
-      if (entitylivingbaseIn.hasPlayerInfo() && !entitylivingbaseIn.isInvisible() && entitylivingbaseIn.isWearing(EnumPlayerModelParts.CAPE) && rl != null) {
+      if (entitylivingbaseIn.hasPlayerInfo() && !entitylivingbaseIn.isInvisible() && entitylivingbaseIn.isWearing(EnumPlayerModelParts.CAPE) && playerHandler.getCapeLocation() != null) {
          ItemStack itemstack = entitylivingbaseIn.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
          if (itemstack.getItem() != Items.ELYTRA) {
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            this.playerRenderer.bindTexture(rl);
+            this.playerRenderer.bindTexture(playerHandler.getCapeLocation());
             GlStateManager.pushMatrix();
             GlStateManager.translate(0.0F, 0.0F, 0.125F);
             double d0 = entitylivingbaseIn.prevChasingPosX + (entitylivingbaseIn.chasingPosX - entitylivingbaseIn.prevChasingPosX) * (double)partialTicks - (entitylivingbaseIn.prevPosX + (entitylivingbaseIn.posX - entitylivingbaseIn.prevPosX) * (double)partialTicks);
@@ -59,7 +60,11 @@ public class CapeLayer implements LayerRenderer<AbstractClientPlayer> {
             GlStateManager.rotate(f3 / 2.0F, 0.0F, 0.0F, 1.0F);
             GlStateManager.rotate(-f3 / 2.0F, 0.0F, 1.0F, 0.0F);
             GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
-            this.playerRenderer.getMainModel().renderCape(0.0625F);
+            this.modelCape.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entitylivingbaseIn);
+            this.modelCape.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+            if (playerHandler.getHasCapeGlint()) {
+               LayerArmorBase.renderEnchantedGlint(this.playerRenderer, entitylivingbaseIn, this.modelCape, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scale);
+            }
             GlStateManager.popMatrix();
          }
       }
