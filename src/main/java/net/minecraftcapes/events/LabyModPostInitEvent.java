168eabd4f297902de1cdaa29f4fbe470cd8e954e
James Harrison <james@fasttortoise.co.uk>
2020-08-23 15:10:24 +0100

    Merged LabyMod and Forge

---
+++ src/main/java/net/minecraftcapes/events/LabyModPostInitEvent.java
@@ -0,0 +1,42 @@
+package net.minecraftcapes.events;
+
+import net.labymod.core.LabyModCore;
+import net.labymod.core_implementation.mc18.RenderPlayerImplementation;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.entity.player.EnumPlayerModelParts;
+import net.minecraftcapes.player.render.CapeLayer;
+import net.minecraftcapes.player.render.Deadmau5;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.lang.reflect.Field;
+
+public class LabyModPostInitEvent {
+
+    public static void init() {
+        MinecraftForge.EVENT_BUS.register(new PlayerEventHandler());
+        Minecraft.getMinecraft().gameSettings.setModelPartEnabled(EnumPlayerModelParts.CAPE, true);
+
+        try {
+            final Class<?> clazz = LabyModCore.getCoreAdapter().getClass();
+            final Field field = clazz.getDeclaredField("renderPlayerImpl");
+            field.setAccessible(true);
+            field.set(LabyModCore.getCoreAdapter(), new RenderPlayerImplementation() {
+                public LayerRenderer<?>[] getLayerRenderers(final RenderPlayer renderPlayer) {
+                LayerRenderer[] layers = super.getLayerRenderers(renderPlayer);
+                layers = ArrayUtils.add(layers, new CapeLayer(renderPlayer));
+                layers = ArrayUtils.add(layers, new Deadmau5(renderPlayer));
+                return layers;
+                }
+            });
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/net/minecraftcapes/proxy/ClientProxy.java
+++ src/main/java/net/minecraftcapes/proxy/ClientProxy.java
@@ -1,11 +1,21 @@
 package net.minecraftcapes.proxy;
 
+import net.minecraftcapes.MinecraftCapes;
 import net.minecraftcapes.events.FMLPostInitEvent;
+import net.minecraftcapes.events.LabyModPostInitEvent;
 
 public class ClientProxy implements IProxy {
 
     @Override
-    public void postInit(){
-        FMLPostInitEvent.init();
+    public void postInit() {
+        try {
+            Class.forName("net.labymod.core.LabyModCore");
+            MinecraftCapes.getLogger().info("Starting in LabyMod Mode");
+            MinecraftCapes.setLabyMod(true);
+            LabyModPostInitEvent.init();
+        } catch (ClassNotFoundException e) {
+            MinecraftCapes.getLogger().info("Starting in Forge Mode");
+            FMLPostInitEvent.init();
+        }
     }
 }
