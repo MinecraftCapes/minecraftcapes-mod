f12d17c3e373ebb617811f6f0edcffcecfbbec8a
James Harrison <james@fasttortoise.co.uk>
2020-09-17 21:20:19 +0100

    Added hooks!

---
+++ src/main/java/net/minecraftcapes/compatibility/ICompatHooks.java
@@ -0,0 +1,9 @@
+package net.minecraftcapes.compatibility;
+
+import net.minecraft.entity.player.PlayerEntity;
+
+public interface ICompatHooks {
+
+    void onPlayerRender(PlayerEntity playerEntity);
+
+}
--- src/main/java/net/minecraftcapes/player/render/CapeLayer.java
+++ src/main/java/net/minecraftcapes/player/render/CapeLayer.java
@@ -26,40 +26,42 @@ public class CapeLayer extends FeatureRenderer<AbstractClientPlayerEntity, Playe
 
 	public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, AbstractClientPlayerEntity abstractClientPlayerEntity, float f, float g, float h, float j, float k, float l) {
 		PlayerHandler playerHandler = PlayerHandler.getFromPlayer(abstractClientPlayerEntity);
-		if (!abstractClientPlayerEntity.isInvisible() && abstractClientPlayerEntity.isPartVisible(PlayerModelPart.CAPE) && playerHandler.getCapeLocation() != null && abstractClientPlayerEntity.getCapeTexture() == null) {
-			ItemStack itemStack = abstractClientPlayerEntity.getEquippedStack(EquipmentSlot.CHEST);
-			if (itemStack.getItem() != Items.ELYTRA) {
-				matrixStack.push();
-				matrixStack.translate(0.0D, 0.0D, 0.125D);
-				double d = MathHelper.lerp((double)h, abstractClientPlayerEntity.field_7524, abstractClientPlayerEntity.field_7500) - MathHelper.lerp((double)h, abstractClientPlayerEntity.prevX, abstractClientPlayerEntity.getX());
-				double e = MathHelper.lerp((double)h, abstractClientPlayerEntity.field_7502, abstractClientPlayerEntity.field_7521) - MathHelper.lerp((double)h, abstractClientPlayerEntity.prevY, abstractClientPlayerEntity.getY());
-				double m = MathHelper.lerp((double)h, abstractClientPlayerEntity.field_7522, abstractClientPlayerEntity.field_7499) - MathHelper.lerp((double)h, abstractClientPlayerEntity.prevZ, abstractClientPlayerEntity.getZ());
-				float n = abstractClientPlayerEntity.prevBodyYaw + (abstractClientPlayerEntity.bodyYaw - abstractClientPlayerEntity.prevBodyYaw);
-				double o = (double)MathHelper.sin(n * 0.017453292F);
-				double p = (double)(-MathHelper.cos(n * 0.017453292F));
-				float q = (float)e * 10.0F;
-				q = MathHelper.clamp(q, -6.0F, 32.0F);
-				float r = (float)(d * o + m * p) * 100.0F;
-				r = MathHelper.clamp(r, 0.0F, 150.0F);
-				float s = (float)(d * p - m * o) * 100.0F;
-				s = MathHelper.clamp(s, -20.0F, 20.0F);
-				if (r < 0.0F) {
-				   r = 0.0F;
-				}
+		if(playerHandler.getShowCape()) {
+			if (!abstractClientPlayerEntity.isInvisible() && abstractClientPlayerEntity.isPartVisible(PlayerModelPart.CAPE) && playerHandler.getCapeLocation() != null && abstractClientPlayerEntity.getCapeTexture() == null) {
+				ItemStack itemStack = abstractClientPlayerEntity.getEquippedStack(EquipmentSlot.CHEST);
+				if (itemStack.getItem() != Items.ELYTRA) {
+					matrixStack.push();
+					matrixStack.translate(0.0D, 0.0D, 0.125D);
+					double d = MathHelper.lerp((double) h, abstractClientPlayerEntity.field_7524, abstractClientPlayerEntity.field_7500) - MathHelper.lerp((double) h, abstractClientPlayerEntity.prevX, abstractClientPlayerEntity.getX());
+					double e = MathHelper.lerp((double) h, abstractClientPlayerEntity.field_7502, abstractClientPlayerEntity.field_7521) - MathHelper.lerp((double) h, abstractClientPlayerEntity.prevY, abstractClientPlayerEntity.getY());
+					double m = MathHelper.lerp((double) h, abstractClientPlayerEntity.field_7522, abstractClientPlayerEntity.field_7499) - MathHelper.lerp((double) h, abstractClientPlayerEntity.prevZ, abstractClientPlayerEntity.getZ());
+					float n = abstractClientPlayerEntity.prevBodyYaw + (abstractClientPlayerEntity.bodyYaw - abstractClientPlayerEntity.prevBodyYaw);
+					double o = (double) MathHelper.sin(n * 0.017453292F);
+					double p = (double) (-MathHelper.cos(n * 0.017453292F));
+					float q = (float) e * 10.0F;
+					q = MathHelper.clamp(q, -6.0F, 32.0F);
+					float r = (float) (d * o + m * p) * 100.0F;
+					r = MathHelper.clamp(r, 0.0F, 150.0F);
+					float s = (float) (d * p - m * o) * 100.0F;
+					s = MathHelper.clamp(s, -20.0F, 20.0F);
+					if (r < 0.0F) {
+						r = 0.0F;
+					}
 
-				float t = MathHelper.lerp(h, abstractClientPlayerEntity.field_7505, abstractClientPlayerEntity.field_7483);
-				q += MathHelper.sin(MathHelper.lerp(h, abstractClientPlayerEntity.prevHorizontalSpeed, abstractClientPlayerEntity.horizontalSpeed) * 6.0F) * 32.0F * t;
-				if (abstractClientPlayerEntity.isInSneakingPose()) {
-				   q += 25.0F;
-				}
+					float t = MathHelper.lerp(h, abstractClientPlayerEntity.field_7505, abstractClientPlayerEntity.field_7483);
+					q += MathHelper.sin(MathHelper.lerp(h, abstractClientPlayerEntity.prevHorizontalSpeed, abstractClientPlayerEntity.horizontalSpeed) * 6.0F) * 32.0F * t;
+					if (abstractClientPlayerEntity.isInSneakingPose()) {
+						q += 25.0F;
+					}
 
-				matrixStack.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(6.0F + r / 2.0F + q));
-				matrixStack.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(s / 2.0F));
-				matrixStack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180.0F - s / 2.0F));
-				VertexConsumer vertexConsumer = ItemRenderer.getArmorVertexConsumer(vertexConsumerProvider, RenderLayer.getEntityTranslucent(playerHandler.getCapeLocation()), false, playerHandler.getHasCapeGlint());
-				this.getContextModel().renderCape(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV);
-				matrixStack.pop();
-			 }
-		  }
+					matrixStack.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(6.0F + r / 2.0F + q));
+					matrixStack.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(s / 2.0F));
+					matrixStack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180.0F - s / 2.0F));
+					VertexConsumer vertexConsumer = ItemRenderer.getArmorVertexConsumer(vertexConsumerProvider, RenderLayer.getEntityTranslucent(playerHandler.getCapeLocation()), false, playerHandler.getHasCapeGlint());
+					this.getContextModel().renderCape(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV);
+					matrixStack.pop();
+				}
+			}
+		}
 	}
 }
