1dc305f3b2cd1eab51fafd746136a829728a8801
James Harrison <james@fasttortoise.co.uk>
2021-07-24 12:57:47 +0100

    Remove old Proxy, bump forge

--- src/main/java/net/minecraftcapes/MinecraftCapes.java
+++ src/main/java/net/minecraftcapes/MinecraftCapes.java
@@ -1,14 +1,20 @@
 package net.minecraftcapes;
 
 import lombok.Getter;
-import net.minecraftcapes.proxy.ClientProxy;
-import net.minecraftcapes.proxy.IProxy;
-import net.minecraftcapes.proxy.ServerProxy;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.PlayerModelPart;
+import net.minecraftcapes.compatibility.CaelusHook;
+import net.minecraftcapes.config.MinecraftCapesConfig;
+import net.minecraftcapes.events.AddLayersEvent;
+import net.minecraftcapes.events.KeyHandlerEvent;
+import net.minecraftcapes.events.PlayerEventHandler;
+import net.minecraftcapes.events.PlayerRenderEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,20 +23,64 @@ import org.apache.logging.log4j.Logger;
 public class MinecraftCapes {
 
 	public static final String MODID = "minecraftcapes";
-
-	@Getter
-	private static final Logger logger = LogManager.getLogger();
-	public static IProxy proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
+	@Getter private static final Logger logger = LogManager.getFormatterLogger("MinecraftCapes");
+	public static KeyBinding menuKey;
 
 	public MinecraftCapes() {
-		getLogger().info("[MinecraftCapes] Initialising");
-
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(proxy::clientSetup);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(proxy::enqueueIMC);
-
-		proxy.init();
-
-		getLogger().info("[MinecraftCapes] Initialised");
+		getLogger().info("Initialising");
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::serverSetup);
+		getLogger().info("Initialised");
 	}
 
+	/**
+	 * This client setup event
+	 * @param event
+	 */
+	public void clientSetup(FMLClientSetupEvent event) {
+		//Loading Config
+		MinecraftCapesConfig.loadConfig();
+
+		//Do Mod compatibility checks :(
+		if(doesClassExist("top.theillusivec4.caelus.api.CaelusApi")) {
+			new CaelusHook();
+		}
+
+		//Register the events
+		MinecraftForge.EVENT_BUS.register(new PlayerEventHandler());
+		MinecraftForge.EVENT_BUS.register(new PlayerRenderEvent());
+		MinecraftForge.EVENT_BUS.register(new KeyHandlerEvent());
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(AddLayersEvent::construct);
+
+		//Try turn on capes
+		Minecraft.getInstance().gameSettings.setModelPartEnabled(PlayerModelPart.CAPE, true);
+
+		//Register the keybinds
+		MinecraftCapes.menuKey = new KeyBinding("key.minecraftcapes.gui", 74, "category.minecraftcapes.gui");
+		ClientRegistry.registerKeyBinding(menuKey);
+	}
+
+	/**
+	 * The Server Started Event
+	 * @param event
+	 */
+	public void serverSetup(FMLDedicatedServerSetupEvent event) {
+		MinecraftCapes.getLogger().error("MinecraftCapes has been loaded on server side. MinecraftCapes is a client only mod. No need to worry about this. You can delete the mod if you wish!");
+	}
+
+	/**
+	 * Check if a class exists
+	 * @param name full name of class
+	 * @return
+	 */
+	private boolean doesClassExist(String name) {
+		try {
+			Class c = Class.forName(name);
+			System.out.println(c);
+			if (c != null) {
+				return true;
+			}
+		} catch (ClassNotFoundException e) {}
+		return false;
+	}
 }
--- src/main/java/net/minecraftcapes/events/KeyHandlerEvent.java
+++ src/main/java/net/minecraftcapes/events/KeyHandlerEvent.java
@@ -3,7 +3,6 @@ package net.minecraftcapes.events;
 import net.minecraft.client.Minecraft;
 import net.minecraftcapes.MinecraftCapes;
 import net.minecraftcapes.gui.MenuScreen;
-import net.minecraftcapes.proxy.ClientProxy;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,7 +14,7 @@ public class KeyHandlerEvent {
     @SubscribeEvent
     public static void onKeyPress(TickEvent.ClientTickEvent event) {
         if(event.phase == TickEvent.Phase.END) {
-            if (ClientProxy.menuKey.isKeyDown()) {
+            if (MinecraftCapes.menuKey.isKeyDown()) {
                 Minecraft.getInstance().displayGuiScreen(new MenuScreen());
             }
         }
