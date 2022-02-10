40e40a7f6fb3622681beaa62843441c88777c44e
James Harrison <james@fasttortoise.co.uk>
2022-02-10 18:06:06 +0000

    Offline support

---
+++ Common/src/main/java/net/minecraftcapes/helpers/MinecraftApi.java
@@ -0,0 +1,70 @@
+package net.minecraftcapes.helpers;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.minecraftcapes.MinecraftCapesConstants;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+public class MinecraftApi {
+    
+    /**
+     * Returns player UUID
+     *
+     * @param username Username to get UUID from
+     * @return Players uuid
+     */
+    public static UUID getUUID(String username) {
+        MinecraftCapesConstants.LOG.warn("Making an API call for {}", username);
+        JsonObject playerElement = getApiData(username);
+        if (playerElement != null) {
+            JsonElement playerUUID = playerElement.get("full_uuid");
+            if (playerUUID != null && !playerUUID.isJsonNull()) {
+                MinecraftCapesConstants.LOG.warn("{} ({}) was found", username, playerUUID);
+                return UUID.fromString(playerUUID.getAsString());
+            }
+        }
+        MinecraftCapesConstants.LOG.warn("{} was not found", username);
+        return null;
+    }
+    
+    /**
+     * Request API call for user data
+     *
+     * @param data The username/uuid to send
+     * @return The response data
+     */
+    private static JsonObject getApiData(String data) {
+        try {
+            URL url = new URL("https://minecraftapi.net/api/v1/profile/" + data);
+            HttpURLConnection httpurlconnection = (HttpURLConnection) url.openConnection();
+            httpurlconnection.setDoInput(true);
+            httpurlconnection.setDoOutput(false);
+            httpurlconnection.connect();
+            
+            if (httpurlconnection.getResponseCode() / 100 == 2) {
+                //Create reader
+                BufferedReader in = new BufferedReader(new InputStreamReader(httpurlconnection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                
+                //Read response
+                while ((inputLine = in.readLine()) != null)
+                    response.append(inputLine);
+                
+                //Convert response to JSON
+                return JsonParser.parseString(response.toString()).getAsJsonObject();
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- Common/src/main/java/net/minecraftcapes/player/DownloadManager.java
+++ Common/src/main/java/net/minecraftcapes/player/DownloadManager.java
@@ -2,28 +2,52 @@ package net.minecraftcapes.player;
 
 import com.google.gson.Gson;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftcapes.MinecraftCapesConstants;
+import net.minecraftcapes.helpers.MinecraftApi;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.UUID;
 
 public class DownloadManager {
     
     public static void prepareDownload(Player player) {
-        //Make sure player is a player and online
-        if(player.getUUID().version() != 4) return;
-        
-        //Make sure we dont have stuff already
+        LocalPlayer localPlayer = Minecraft.getInstance().player;
+
+        //Make sure player is online and not the local player in offline mode
+        if(player.getUUID().version() != 4 && (localPlayer != null && !localPlayer.getUUID().equals(player.getUUID()))) return;
+    
         PlayerHandler playerHandler = PlayerHandler.getFromPlayer(player);
-        if(playerHandler == null || playerHandler.getHasInfo()) return;
-        
-        //Download!
-        DownloadManager.downloadProfile(playerHandler);
+        //Lets get the local players offline cape
+        if(player.getUUID().version() != 4) {
+            //Stop any more processing
+            playerHandler.setHasInfo(true);
+            
+            //Get UUID from API off main thread
+            Thread playerDownload = new Thread(() -> {
+                UUID uuid = MinecraftApi.getUUID(player.getScoreboardName());
+                if(uuid == null) return;
+                playerHandler.setPlayerUUID(uuid);
+                
+                //Download!
+                DownloadManager.downloadProfile(playerHandler);
+            });
+            playerDownload.setDaemon(true);
+            playerDownload.start();
+        } else {
+            //Make sure we don't have stuff already
+            if(playerHandler.getHasInfo()) return;
+            
+            //Download!
+            DownloadManager.downloadProfile(playerHandler);
+        }
     }
     
     public static void downloadProfile(PlayerHandler playerHandler) {
@@ -37,7 +61,7 @@ public class DownloadManager {
                 httpurlconnection.connect();
                 
                 if (httpurlconnection.getResponseCode() / 100 == 2) {
-                    Reader reader = new InputStreamReader(httpurlconnection.getInputStream(), "UTF-8");
+                    Reader reader = new InputStreamReader(httpurlconnection.getInputStream(), StandardCharsets.UTF_8);
                     ProfileResult profileResult = new Gson().fromJson(reader, ProfileResult.class);
                     reader.close();
                     
@@ -62,7 +86,7 @@ public class DownloadManager {
         playerDownload.start();
     }
     
-    class ProfileResult {
+    static class ProfileResult {
         private boolean capeGlint = false;
         private boolean upsideDown = false;
         private Map<String, String> textures = null;
