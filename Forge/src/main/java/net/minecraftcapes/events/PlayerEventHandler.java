192198a94590facfd016cf82b433d7ef62c8027e
James Harrison <james@fasttortoise.co.uk>
2022-07-13 14:57:07 +0100

    Fix forge crashes

--- Forge/src/main/java/net/minecraftcapes/events/PlayerEventHandler.java
+++ Forge/src/main/java/net/minecraftcapes/events/PlayerEventHandler.java
@@ -2,15 +2,14 @@ package net.minecraftcapes.events;
 
 import net.minecraft.world.entity.player.Player;
 import net.minecraftcapes.player.DownloadManager;
-import net.minecraftcapes.player.PlayerHandler;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class PlayerEventHandler {
     
     @SubscribeEvent
-    public void onPlayerJoin(EntityJoinWorldEvent event) {
-        if(event.getEntity() instanceof Player && event.getWorld().isClientSide()) {
+    public void onPlayerJoin(EntityJoinLevelEvent event) {
+        if(event.getEntity() instanceof Player && event.getLevel().isClientSide()) {
             DownloadManager.prepareDownload((Player) event.getEntity(), false);
         }
     }
