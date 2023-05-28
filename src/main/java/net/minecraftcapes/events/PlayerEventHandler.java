7b70e0fb9372384b24302f8272f5fc57e1ea828c
James Harrison <james@fasttortoise.co.uk>
2023-05-28 16:09:53 +0100

    Final build

--- src/main/java/net/minecraftcapes/events/PlayerEventHandler.java
+++ src/main/java/net/minecraftcapes/events/PlayerEventHandler.java
@@ -32,7 +32,7 @@ public class PlayerEventHandler {
 		Thread playerDownload = new Thread(() -> {
 			try {
 				MinecraftCapes.getLogger().debug("Getting profile for {}", playerHandler.getPlayerUUID());
-				URL url = new URL("https://minecraftcapes.net/profile/" + playerHandler.getPlayerUUID().toString().replace("-", ""));
+				URL url = new URL("https://api.minecraftcapes.net/profile/" + playerHandler.getPlayerUUID().toString().replace("-", ""));
 				HttpURLConnection httpurlconnection = (HttpURLConnection) url.openConnection(Minecraft.getInstance().getProxy());
 				httpurlconnection.setDoInput(true);
 				httpurlconnection.setDoOutput(false);
