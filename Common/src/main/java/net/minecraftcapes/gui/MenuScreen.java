cad2ccad22956c37b962ecdb0f8c49250911c24f
James Harrison <james@fasttortoise.co.uk>
2023-06-03 18:32:25 +0100

    Fix 1.19.1

--- Common/src/main/java/net/minecraftcapes/gui/MenuScreen.java
+++ Common/src/main/java/net/minecraftcapes/gui/MenuScreen.java
@@ -3,13 +3,14 @@ package net.minecraftcapes.gui;
 import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Quaternion;
+import com.mojang.math.Vector3f;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.CycleButton;
 import net.minecraft.client.gui.screens.ConfirmLinkScreen;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.network.chat.CommonComponents;
@@ -17,26 +18,25 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftcapes.config.MinecraftCapesConfig;
 import net.minecraftcapes.player.DownloadManager;
-import org.joml.Quaternionf;
 
 public class MenuScreen extends Screen {
-
+    
     public MenuScreen() {
         super(Component.translatable("category.minecraftcapes.gui"));
     }
-
+    
     protected void init() {
         int i = 0;
         
         //Reload Profile
-        this.addRenderableWidget(Button.builder(Component.nullToEmpty("Open MinecraftCapes"), (button) -> {
+        this.addRenderableWidget(new Button(this.width / 3 * 2 - 75, this.height / 3, 150, 20, Component.nullToEmpty("Open MinecraftCapes"), (button) -> {
             this.minecraft.setScreen(new ConfirmLinkScreen((shouldOpen) -> {
                 if(shouldOpen) {
                     Util.getPlatform().openUri("https://minecraftcapes.net");
                 }
                 this.minecraft.setScreen(this);
             }, "https://minecraftcapes.net", true));
-        }).bounds(this.width / 3 * 2 - 75, this.height / 3, 150, 20).build());
+        }));
         i++;
         
         //For custom cape/ears onto new line
@@ -47,29 +47,30 @@ public class MenuScreen extends Screen {
             MinecraftCapesConfig.setCapeVisible(enabled);
         }));
         i++;
-
+        
         //Custom Ears
         this.addRenderableWidget(CycleButton.onOffBuilder(MinecraftCapesConfig.isEarsVisible()).create(this.width / 3 * 2 - 155 + i % 2 * 160, this.height / 3 + 24 * (i >> 1), 150, 20, getButtonString("Custom Ears"), (button, enabled) -> {
             MinecraftCapesConfig.setEarsVisible(enabled);
         }));
         i++;
-
+        
         //Force Reload Profile to get an extra line
         i++;
-
+        
         //Reload Profile
-        this.addRenderableWidget(Button.builder(Component.nullToEmpty("Reload Profile"), (button) -> {
+        this.addRenderableWidget(new Button(this.width / 3 * 2 - 75, this.height / 3 + 24 * (i >> 1), 150, 20, Component.nullToEmpty("Reload Profile"), (button) -> {
             DownloadManager.prepareDownload(this.minecraft.player, true);
-        }).bounds(this.width / 3 * 2 - 75, this.height / 3 + 24 * (i >> 1), 150, 20).build());
+        }));
         i++;
-
+        
         //Done
-        this.addRenderableWidget(Button.builder(CommonComponents.GUI_DONE, (button) -> {
+        this.addRenderableWidget(new Button(this.width / 3 * 2 - 100, this.height / 3 + 24 * (i >> 1), 200, 20, CommonComponents.GUI_DONE, (button) -> {
             this.minecraft.setScreen(null);
-        }).bounds(this.width / 3 * 2 - 100, this.height / 3 + 24 * (i >> 1), 200, 20).build());
+        }));
     }
-
+    
     public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
+        int guiScale = (int) this.minecraft.getWindow().getGuiScale();
         this.renderBackground(poseStack);
         drawCenteredString(poseStack, this.font, this.title, this.width / 2, 20, 0xFFFFFF);
         renderPlayer(
@@ -80,20 +81,18 @@ public class MenuScreen extends Screen {
         );
         super.render(poseStack, mouseX, mouseY, partialTicks);
     }
-    
     public static void renderPlayer(int $$0, int $$1, int $$2, LivingEntity livingEntity) {
-        float $$6 = 0f;
         float $$7 = 0f;
         PoseStack $$8 = RenderSystem.getModelViewStack();
         $$8.pushPose();
-        $$8.translate((float)$$0, (float)$$1, 1050.0F);
+        $$8.translate((double)$$0, (double)$$1, 1050.0);
         $$8.scale(1.0F, 1.0F, -1.0F);
         RenderSystem.applyModelViewMatrix();
         PoseStack $$9 = new PoseStack();
-        $$9.translate(0.0F, 0.0F, 1000.0F);
+        $$9.translate(0.0, 0.0, 1000.0);
         $$9.scale((float)$$2, (float)$$2, (float)$$2);
-        Quaternionf $$10 = (new Quaternionf()).rotateZ(3.1415927F);
-        Quaternionf $$11 = (new Quaternionf()).rotateX($$7 * 20.0F * 0.017453292F);
+        Quaternion $$10 = Vector3f.ZP.rotationDegrees(180.0F);
+        Quaternion $$11 = Vector3f.XP.rotationDegrees($$7 * 20.0F);
         $$10.mul($$11);
         $$9.mulPose($$10);
         float $$12 = livingEntity.yBodyRot;
@@ -101,14 +100,14 @@ public class MenuScreen extends Screen {
         float $$14 = livingEntity.getXRot();
         float $$15 = livingEntity.yHeadRotO;
         float $$16 = livingEntity.yHeadRot;
-        livingEntity.yBodyRot = 0f;
-        livingEntity.setYRot(0f);
+        livingEntity.yBodyRot = 0f; //180f
+        livingEntity.setYRot(0F); //180f
         livingEntity.setXRot(0f);
         livingEntity.yHeadRot = livingEntity.getYRot();
         livingEntity.yHeadRotO = livingEntity.getYRot();
         Lighting.setupForEntityInInventory();
         EntityRenderDispatcher $$17 = Minecraft.getInstance().getEntityRenderDispatcher();
-        $$11.conjugate();
+        $$11.conj();
         $$17.overrideCameraOrientation($$11);
         $$17.setRenderShadow(false);
         MultiBufferSource.BufferSource $$18 = Minecraft.getInstance().renderBuffers().bufferSource();
--- Common/src/main/java/net/minecraftcapes/mixin/MixinClientPacketListener.java
+++ Common/src/main/java/net/minecraftcapes/mixin/MixinClientPacketListener.java
@@ -2,10 +2,8 @@ package net.minecraftcapes.mixin;
 
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.multiplayer.PlayerInfo;
-import net.minecraft.network.TickablePacketListener;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoRemovePacket;
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoPacket;
 import net.minecraftcapes.player.DownloadManager;
 import net.minecraftcapes.player.PlayerHandler;
 import org.spongepowered.asm.mixin.Final;
@@ -19,23 +17,21 @@ import java.util.Map;
 import java.util.UUID;
 
 @Mixin(ClientPacketListener.class)
-public abstract class MixinClientPacketListener implements TickablePacketListener, ClientGamePacketListener {
+public abstract class MixinClientPacketListener implements ClientGamePacketListener {
 
     @Shadow
     @Final
     private Map<UUID, PlayerInfo> playerInfoMap;
-
-    @Inject(method = "handlePlayerInfoUpdate", at = @At("RETURN"))
-    public void handlePlayerInfoUpdate(ClientboundPlayerInfoUpdatePacket updatePacket, CallbackInfo ci) {
-        updatePacket.newEntries().forEach(entry -> {
-            PlayerInfo playerInfo = this.playerInfoMap.get(entry.profileId());
-            DownloadManager.prepareDownload(playerInfo, false);
-        });
+    
+    @Inject(method = "handlePlayerInfo", at = @At("RETURN"))
+    public void handlePlayerInfo(ClientboundPlayerInfoPacket infoPacket, CallbackInfo ci) {
+        if(infoPacket.getAction() == ClientboundPlayerInfoPacket.Action.REMOVE_PLAYER) {
+            infoPacket.getEntries().forEach(playerUpdate -> PlayerHandler.remove(playerUpdate.getProfile().getId()));
+        } else {
+            infoPacket.getEntries().forEach(entry -> {
+                PlayerInfo playerInfo = this.playerInfoMap.get(entry.getProfile().getId());
+                DownloadManager.prepareDownload(playerInfo, false);
+            });
+        }
     }
-
-    @Inject(method = "handlePlayerInfoRemove", at = @At("RETURN"))
-    public void handlePlayerInfoRemove(ClientboundPlayerInfoRemovePacket removePacket, CallbackInfo ci) {
-        removePacket.profileIds().forEach(PlayerHandler::remove);
-    }
-
 }
