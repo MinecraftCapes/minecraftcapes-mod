0c22a96b38f5e21cfb9fdd80f350e0ae0161897b
James Harrison <james@fasttortoise.co.uk>
2024-05-14 20:59:56 +0100

    Update to 1.20.5

--- NeoForge/src/main/java/net/minecraftcapes/neoforge/client/ClientForgeEvents.java
+++ NeoForge/src/main/java/net/minecraftcapes/neoforge/client/ClientForgeEvents.java
@@ -7,32 +7,19 @@ import net.minecraftcapes.MinecraftCapes;
 import net.minecraftcapes.gui.MenuScreen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.settings.KeyConflictContext;
 import net.neoforged.neoforge.common.util.Lazy;
-import net.neoforged.neoforge.event.TickEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import org.lwjgl.glfw.GLFW;
 
-@Mod.EventBusSubscriber(modid = MinecraftCapes.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+@EventBusSubscriber(modid = MinecraftCapes.MOD_ID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
 public class ClientForgeEvents {
     
-    public static final Lazy<KeyMapping> KEY_MAPPING = Lazy.of(() -> new KeyMapping(
-            "key." + MinecraftCapes.MOD_ID + ".gui",
-            KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_J,
-            "category." + MinecraftCapes.MOD_ID + ".gui"
-    ));
+
     
     /**
      * Register the client tick for key listening
      */
-    @SubscribeEvent
-    public static void onClickTick(TickEvent.ClientTickEvent event) {
-        if(event.phase == TickEvent.Phase.END) {
-            if (KEY_MAPPING.get().consumeClick()) {
-                Minecraft.getInstance().setScreen(new MenuScreen());
-            }
-        }
-    }
+
 }
