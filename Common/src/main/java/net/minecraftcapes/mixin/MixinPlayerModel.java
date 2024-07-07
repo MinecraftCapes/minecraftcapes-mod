623c1cee55c8e439e5420b7db2a5d568df2b4437
James Harrison <james@fasttortoise.co.uk>
2024-07-07 16:39:11 +0100

    Fix issues with ears rendering double with some mods

--- Common/src/main/java/net/minecraftcapes/mixin/MixinPlayerModel.java
+++ Common/src/main/java/net/minecraftcapes/mixin/MixinPlayerModel.java
@@ -24,9 +24,10 @@ public abstract class MixinPlayerModel extends HumanoidModel {
     private static void createMesh(CubeDeformation cubeDeformation, boolean slim, CallbackInfoReturnable<MeshDefinition> cir) {
         MeshDefinition meshDefinition = cir.getReturnValue();
         meshDefinition.getRoot().addOrReplaceChild("ear", CubeListBuilder.create(), PartPose.ZERO);
+        
         PartDefinition partDefinition = meshDefinition.getRoot().getChild("ear");
-        partDefinition.addOrReplaceChild("left_ear", CubeListBuilder.create().texOffs(0, 0).addBox(1.5F, -10.5F, -1.0F, 6, 6, 1, cubeDeformation, 0.21875F, 0.109375F), PartPose.ZERO);
-        partDefinition.addOrReplaceChild("right_ear", CubeListBuilder.create().texOffs(0, 0).addBox(-7.5F, -10.5F, -1.0F, 6, 6, 1, cubeDeformation, 0.21875F, 0.109375F), PartPose.ZERO);
+        partDefinition.addOrReplaceChild("mcc_left_ear", CubeListBuilder.create().texOffs(0, 0).addBox(1.5F, -10.5F, -1.0F, 6, 6, 1, cubeDeformation, 0.21875F, 0.109375F), PartPose.ZERO);
+        partDefinition.addOrReplaceChild("mcc_right_ear", CubeListBuilder.create().texOffs(0, 0).addBox(-7.5F, -10.5F, -1.0F, 6, 6, 1, cubeDeformation, 0.21875F, 0.109375F), PartPose.ZERO);
         cir.setReturnValue(meshDefinition);
     }
 }
