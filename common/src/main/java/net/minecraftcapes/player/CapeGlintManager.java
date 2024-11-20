b3a04845d2528b1fc33fc8ef525163572713b18b
James Harrison <james@fasttortoise.co.uk>
2024-11-20 19:17:33 +0000

    Work on 12.4.0 upgrade

--- common/src/main/java/net/minecraftcapes/player/CapeGlintManager.java
+++ common/src/main/java/net/minecraftcapes/player/CapeGlintManager.java
@@ -7,7 +7,7 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraftcapes.MinecraftCapes;
 public class CapeGlintManager {
     public static RenderType CAPE_GLINT = MinecraftCapes.getCapeGlint();
-    public static VertexConsumer getCapeBuffer(MultiBufferSource bufferSource, RenderType renderType, boolean hasGlint) {
-        return hasGlint ? VertexMultiConsumer.create(bufferSource.getBuffer(CAPE_GLINT), bufferSource.getBuffer(renderType)) : bufferSource.getBuffer(renderType);
+    public static VertexConsumer getCapeBuffer(MultiBufferSource bufferSource, RenderType renderType) {
+        return VertexMultiConsumer.create(bufferSource.getBuffer(CAPE_GLINT), bufferSource.getBuffer(renderType));
     }
 }
--- common/src/main/java/net/minecraftcapes/player/DownloadManager.java
+++ common/src/main/java/net/minecraftcapes/player/DownloadManager.java
@@ -2,9 +2,7 @@ package net.minecraftcapes.player;
 
 import com.google.gson.Gson;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftcapes.MinecraftCapes;
 import net.minecraftcapes.helpers.MinecraftApi;
 
@@ -17,14 +15,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.UUID;
 
-public class DownloadManager {
+import static net.minecraftcapes.MinecraftCapes.MINECRAFT_VERSION;
 
-    public static void prepareDownload(Player player, boolean doRefresh) {
-        prepareDownload(player.getUUID(), player.getGameProfile().getName(), doRefresh);
-    }
-    public static void prepareDownload(PlayerInfo playerInfo, boolean doRefresh) {
-        prepareDownload(playerInfo.getProfile().getId(), playerInfo.getProfile().getName(), doRefresh);
-    }
+public class DownloadManager {
 
     /**
      * Prepares the down
@@ -32,7 +25,7 @@ public class DownloadManager {
      * @param playerName The players name
      * @param doRefresh Whether we are forcing an overwrite
      */
-    private static void prepareDownload(UUID playerUUID, String playerName, boolean doRefresh) {
+    public static void prepareDownload(UUID playerUUID, String playerName, boolean doRefresh) {
         LocalPlayer localPlayer = Minecraft.getInstance().player;
 
         //Make sure player is online and not the local player in offline mode
@@ -78,6 +71,7 @@ public class DownloadManager {
                 MinecraftCapes.getLogger().debug("Getting profile for {}", playerHandler.getPlayerUUID());
                 URL url = new URL("https://api.minecraftcapes.net/profile/" + playerHandler.getPlayerUUID().toString().replace("-", ""));
                 HttpURLConnection httpurlconnection = (HttpURLConnection) url.openConnection(Minecraft.getInstance().getProxy());
+                httpurlconnection.setRequestProperty("User-Agent", "minecraftcapes-mod/" + MINECRAFT_VERSION);
                 httpurlconnection.setDoInput(true);
                 httpurlconnection.setDoOutput(false);
                 httpurlconnection.connect();
