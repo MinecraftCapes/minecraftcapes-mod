5a3bda74ac319ceec08e7ae058a87c287ca89bd4
James Harrison <james@fasttortoise.co.uk>
2025-11-18 00:29:52 +0000

    Fix transparency

--- src/main/java/net/minecraftcapes/mixin/MixinRenderPlayer.java
+++ src/main/java/net/minecraftcapes/mixin/MixinRenderPlayer.java
@@ -1,9 +1,9 @@
 package net.minecraftcapes.mixin;
 
-import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftcapes.player.PlayerHandler;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,19 +11,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(RenderPlayer.class)
-public abstract class MixinRenderPlayer extends RendererLivingEntity {
+@Mixin(RendererLivingEntity.class)
+public abstract class MixinRenderPlayer extends Render {
 
-    public MixinRenderPlayer(ModelBase p_i1261_1_, float p_i1261_2_) {
-        super(p_i1261_1_, p_i1261_2_);
-    }
-
-    @Inject(method = "rotateCorpse(Lnet/minecraft/client/entity/AbstractClientPlayer;FFF)V", at = @At(value = "HEAD"))
-    public void renderUpsidedown(AbstractClientPlayer p_77043_1_, float p_77043_2_, float p_77043_3_, float p_77043_4_, CallbackInfo ci) {
-        PlayerHandler playerHandler = PlayerHandler.get(p_77043_1_.getUniqueID());
-        if(playerHandler.isUpsideDown()) {
-            GL11.glTranslatef(0.0F, p_77043_1_.height + 0.1F, 0.0F);
-            GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+    @Inject(method = "rotateCorpse", at = @At(value = "RETURN"))
+    public void renderUpsidedown(EntityLivingBase entityLivingBase, float p_77043_2_, float p_77043_3_, float p_77043_4_, CallbackInfo ci) {
+        if(entityLivingBase instanceof EntityPlayer) {
+            PlayerHandler playerHandler = PlayerHandler.get(entityLivingBase.getUniqueID());
+            if (playerHandler.isUpsideDown()) {
+                GL11.glTranslatef(0.0F, entityLivingBase.height + 0.1F, 0.0F);
+                GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+            }
         }
     }
 }
