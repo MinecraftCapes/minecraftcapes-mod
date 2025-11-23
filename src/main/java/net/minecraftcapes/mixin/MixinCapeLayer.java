700f33e1369b75c70e25a3de72d62be8e68d5817
James Harrison <james@fasttortoise.co.uk>
2025-11-23 19:27:07 +0000

    Fix glint method

--- src/main/java/net/minecraftcapes/mixin/MixinCapeLayer.java
+++ src/main/java/net/minecraftcapes/mixin/MixinCapeLayer.java
@@ -1,9 +1,12 @@
 package net.minecraftcapes.mixin;
 
 import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.client.renderer.entity.layers.LayerArmorBase;
 import net.minecraft.client.renderer.entity.layers.LayerCape;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftcapes.config.MinecraftCapesConfig;
@@ -23,53 +26,20 @@ public class MixinCapeLayer {
     @Shadow
     private RenderPlayer playerRenderer;
 
-    @Unique
-    private static final ResourceLocation minecraftcapes$ENCHANTED_ITEM_GLINT_RES = new ResourceLocation("textures/misc/enchanted_item_glint.png");
-
-    @Inject(method = "doRenderLayer(Lnet/minecraft/client/entity/AbstractClientPlayer;FFFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPlayer;renderCape(F)V", shift = At.Shift.AFTER), cancellable = true)
-    public void addCapeGlint(AbstractClientPlayer entitylivingbaseIn, float p_177141_2_, float p_177141_3_, float partialTicks, float p_177141_5_, float p_177141_6_, float p_177141_7_, float scale, CallbackInfo ci) {
+    @Inject(method = "doRenderLayer(Lnet/minecraft/client/entity/AbstractClientPlayer;FFFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPlayer;renderCape(F)V", shift = At.Shift.AFTER))
+    public void addCapeGlint(AbstractClientPlayer entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale, CallbackInfo ci) {
         // Retrieve the player handler from playerRenderState
         PlayerHandler playerHandler = PlayerHandler.get(entitylivingbaseIn.getUniqueID());
 
         // Redirect to custom buffer if player has cape glint, otherwise use the default buffer
         if (MinecraftCapesConfig.isCapeVisible() && playerHandler.getHasCapeGlint()) {
-            minecraftcapes$renderEchantmentGlint(entitylivingbaseIn, partialTicks);
+            LayerArmorBase.renderEnchantedGlint(this.playerRenderer, entitylivingbaseIn, new ModelBase() {
+                @Override
+                public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+                    playerRenderer.getMainModel().renderCape(0.0625F);
+                }
+            }, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scale);
         }
-    }
-
-    @Unique
-    private void minecraftcapes$renderEchantmentGlint(EntityLivingBase entitylivingbaseIn, float p_177183_5_) {
-        float f = (float)entitylivingbaseIn.ticksExisted + p_177183_5_;
-        this.playerRenderer.bindTexture(minecraftcapes$ENCHANTED_ITEM_GLINT_RES);
-        GlStateManager.enableBlend();
-        GlStateManager.depthFunc(514);
-        GlStateManager.depthMask(false);
-        float f1 = 0.5F;
-        GlStateManager.color(f1, f1, f1, 1.0F);
-
-        for (int i = 0; i < 2; ++i)
-        {
-            GlStateManager.disableLighting();
-            GlStateManager.blendFunc(768, 1);
-            float f2 = 0.76F;
-            GlStateManager.color(0.5F * f2, 0.25F * f2, 0.8F * f2, 1.0F);
-            GlStateManager.matrixMode(5890);
-            GlStateManager.loadIdentity();
-            float f3 = 0.33333334F;
-            GlStateManager.scale(f3, f3, f3);
-            GlStateManager.rotate(30.0F - (float)i * 60.0F, 0.0F, 0.0F, 1.0F);
-            GlStateManager.translate(0.0F, f * (0.001F + (float)i * 0.003F) * 20.0F, 0.0F);
-            GlStateManager.matrixMode(5888);
-            this.playerRenderer.getMainModel().renderCape(0.0625F);
-        }
-
-        GlStateManager.matrixMode(5890);
-        GlStateManager.loadIdentity();
-        GlStateManager.matrixMode(5888);
-        GlStateManager.enableLighting();
-        GlStateManager.depthMask(true);
-        GlStateManager.depthFunc(515);
-        GlStateManager.disableBlend();
     }
 
 }
