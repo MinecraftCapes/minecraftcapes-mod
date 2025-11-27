81918a91d44c736bf9e39cc7d87b9d1c4b71dbdc
James Harrison <james@fasttortoise.co.uk>
2025-11-27 17:25:09 +0000

    Update to new API

--- common/src/main/java/net/minecraftcapes/mixin/MixinCapeLayer.java
+++ common/src/main/java/net/minecraftcapes/mixin/MixinCapeLayer.java
@@ -1,30 +1,47 @@
 package net.minecraftcapes.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.ElytraModel;
+import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.CapeLayer;
-import net.minecraftcapes.player.CapeGlintManager;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraftcapes.config.MinecraftCapesConfig;
 import net.minecraftcapes.player.PlayerHandler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CapeLayer.class)
-public class MixinCapeLayer {
+public abstract class MixinCapeLayer extends RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
 
-    @Redirect(method = "render*", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/MultiBufferSource;getBuffer(Lnet/minecraft/client/renderer/RenderType;)Lcom/mojang/blaze3d/vertex/VertexConsumer;"))
-    private VertexConsumer addCapeGlint(MultiBufferSource bufferSource, RenderType renderType, @Local(argsOnly = true) AbstractClientPlayer abstractClientPlayer) {
-        // Retrieve the player handler from playerRenderState
-        PlayerHandler playerHandler = PlayerHandler.get(abstractClientPlayer);
+    @Unique
+    private PlayerHandler minecraftcapes$playerHandler;
 
-        // Redirect to custom buffer if player has cape glint, otherwise use the default buffer
-        if (playerHandler.getHasCapeGlint()) {
-            return CapeGlintManager.getCapeBuffer(bufferSource, RenderType.armorCutoutNoCull(abstractClientPlayer.getSkin().capeTexture()));
+    public MixinCapeLayer(RenderLayerParent<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> renderLayerParent) {
+        super(renderLayerParent);
+    }
+
+    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/player/AbstractClientPlayer;FFFFFF)V", at = @At(value = "HEAD"))
+    public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, AbstractClientPlayer abstractClientPlayer, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
+        minecraftcapes$playerHandler = PlayerHandler.get(abstractClientPlayer.getUUID());
+    }
+
+    @Redirect(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/player/AbstractClientPlayer;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/MultiBufferSource;getBuffer(Lnet/minecraft/client/renderer/RenderType;)Lcom/mojang/blaze3d/vertex/VertexConsumer;"))
+    public VertexConsumer minecraftcapes$renderGlint(MultiBufferSource instance, RenderType renderType) {
+        if(MinecraftCapesConfig.isCapeVisible()) {
+            return ItemRenderer.getArmorFoilBuffer(instance, RenderType.armorCutoutNoCull(minecraftcapes$playerHandler.getCapeLocation()), minecraftcapes$playerHandler.getHasCapeGlint());
         } else {
-            return bufferSource.getBuffer(RenderType.armorCutoutNoCull(abstractClientPlayer.getSkin().capeTexture()));
+            return instance.getBuffer(renderType);
         }
     }
 }
