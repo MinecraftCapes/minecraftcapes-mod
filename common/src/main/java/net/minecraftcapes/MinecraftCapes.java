e89e01191aa9c6f5e0947ad0cead7f837f0bc534
James Harrison <james@fasttortoise.co.uk>
2025-11-27 23:00:39 +0000

    Update to new API

--- common/src/main/java/net/minecraftcapes/MinecraftCapes.java
+++ common/src/main/java/net/minecraftcapes/MinecraftCapes.java
@@ -1,41 +1,42 @@
 package net.minecraftcapes;
 
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.VertexFormat;
+import com.mojang.blaze3d.platform.InputConstants;
 import lombok.Getter;
 import net.minecraft.SharedConstants;
-import net.minecraft.client.renderer.RenderPipelines;
-import net.minecraft.client.renderer.RenderStateShard;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraft.util.TriState;
+import net.minecraft.client.KeyMapping;
 import net.minecraftcapes.config.MinecraftCapesConfig;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
 
 public class MinecraftCapes {
     
     public static final String MOD_ID = "minecraftcapes";
     public static final String MOD_NAME = "MinecraftCapes";
     public static final String MINECRAFT_VERSION = SharedConstants.getCurrentVersion().getName();
-
-    @Getter
-    private static final Logger logger = LogManager.getLogger(MOD_NAME);
-    @Getter
-    protected static RenderType capeGlint = RenderType.create("cape_glint",
-            1536,
-            RenderPipelines.GLINT,
-            RenderType.CompositeState.builder()
-                    .setTextureState(
-                            new RenderStateShard.TextureStateShard(ItemRenderer.ENCHANTED_GLINT_ITEM, TriState.TRUE, false))
-                    .setTexturingState(RenderType.ENTITY_GLINT_TEXTURING)
-                    .setLayeringState(RenderType.VIEW_OFFSET_Z_LAYERING)
-                    .createCompositeState(false));
     
-    public static void onEnable() {
+    @Getter
+    private static Path configDir;
+    
+    @Getter
+    private static final Logger logger = LoggerFactory.getLogger(MOD_NAME);
+    
+    public static final KeyMapping KEY_MAPPING = new KeyMapping(
+            "Open GUI",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_J,
+            "MinecraftCapes"
+    );
+    
+    public static void onEnable(Path configDir) {
         MinecraftCapes.getLogger().info("Initialising");
         
         //Loading Config
+        MinecraftCapes.configDir = configDir.resolve(MOD_ID);
         MinecraftCapesConfig.loadConfig();
+        
+        MinecraftCapes.getLogger().info("Initialised");
     }
 }
---
+++ common/src/main/java/net/minecraftcapes/mixin/MixinPlayerRenderState.java
@@ -0,0 +1,64 @@
+package net.minecraftcapes.mixin;
+
+import net.minecraft.client.renderer.entity.state.PlayerRenderState;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftcapes.player.ExtendedRenderState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(PlayerRenderState.class)
+public class MixinPlayerRenderState implements ExtendedRenderState {
+    
+    @Unique
+    private boolean minecraftcapes$capeEnabled;
+    
+    @Unique
+    private boolean minecraftcapes$capeGlint;
+    
+    @Unique
+    private boolean minecraftcapes$earsEnabled;
+    
+    @Unique
+    private ResourceLocation minecraftcapes$earsTexture;
+    
+    @Override
+    public void minecraftcapes$setCapeEnabled(boolean value) {
+        this.minecraftcapes$capeEnabled = value;
+    }
+    
+    @Override
+    public boolean minecraftcapes$getCapeEnabled() {
+        return this.minecraftcapes$capeEnabled;
+    }
+    
+    @Override
+    public void minecraftcapes$setGapeGlint(boolean value) {
+        this.minecraftcapes$capeGlint = value;
+    }
+    
+    @Override
+    public boolean minecraftcapes$hasCapeGlint() {
+        return this.minecraftcapes$capeGlint;
+    }
+    
+    @Override
+    public void minecraftcapes$setEarsEnabled(boolean value) {
+        this.minecraftcapes$earsEnabled = value;
+    }
+    
+    @Override
+    public boolean minecraftcapes$getEarsEnabled() {
+        return this.minecraftcapes$earsEnabled;
+    }
+    
+    @Override
+    public void minecraftcapes$setEarsTexture(ResourceLocation value) {
+        this.minecraftcapes$earsTexture = value;
+    }
+    
+    @Override
+    public ResourceLocation minecraftcapes$getEarsTexture() {
+        return this.minecraftcapes$earsTexture;
+    }
+    
+}
