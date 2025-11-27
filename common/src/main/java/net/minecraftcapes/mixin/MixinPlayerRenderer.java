4bee3ae7a82066a7ec96f5c4ee4d7976e55616be
James Harrison <james@fasttortoise.co.uk>
2025-11-27 22:49:42 +0000

    Update to new API

---
+++ common/src/main/java/net/minecraftcapes/mixin/MixinPlayerRenderer.java
@@ -0,0 +1,29 @@
+package net.minecraftcapes.mixin;
+
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.client.renderer.entity.state.PlayerRenderState;
+import net.minecraftcapes.player.ExtendedRenderState;
+import net.minecraftcapes.config.MinecraftCapesConfig;
+import net.minecraftcapes.player.PlayerHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerRenderer.class)
+public class MixinPlayerRenderer {
+    
+    @Inject(method = "extractRenderState(Lnet/minecraft/client/player/AbstractClientPlayer;Lnet/minecraft/client/renderer/entity/state/PlayerRenderState;F)V", at = @At(value = "TAIL"))
+    public void extractRenderState(AbstractClientPlayer abstractClientPlayer, PlayerRenderState playerRenderState, float p_364121_, CallbackInfo ci) {
+        ExtendedRenderState extendedRenderState = (ExtendedRenderState) playerRenderState;
+        PlayerHandler playerHandler = PlayerHandler.get(abstractClientPlayer.getUUID());
+        if(playerHandler.getHasInfo()) {
+            playerRenderState.isUpsideDown = playerHandler.isUpsideDown();
+            extendedRenderState.minecraftcapes$setCapeEnabled(MinecraftCapesConfig.isCapeVisible());
+            extendedRenderState.minecraftcapes$setGapeGlint(playerHandler.getHasCapeGlint());
+            extendedRenderState.minecraftcapes$setEarsEnabled(MinecraftCapesConfig.isEarsVisible());
+            extendedRenderState.minecraftcapes$setEarsTexture(playerHandler.getEarLocation());
+        }
+    }
+}
---
+++ neoforge/src/main/java/net/minecraftcapes/neoforge/events/KeyHandlerEvent.java
@@ -0,0 +1,21 @@
+package net.minecraftcapes.neoforge.events;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftcapes.MinecraftCapes;
+import net.minecraftcapes.gui.MenuScreen;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+
+@EventBusSubscriber(modid = MinecraftCapes.MOD_ID, value = Dist.CLIENT)
+public class KeyHandlerEvent {
+
+    @SubscribeEvent
+    public static void onKeyPress(ClientTickEvent.Post event) {
+        if (MinecraftCapes.KEY_MAPPING.consumeClick()) {
+            Minecraft.getInstance().setScreen(new MenuScreen());
+        }
+    }
+
+}
