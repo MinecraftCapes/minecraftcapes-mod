2e16f4f7b8b19ac80f54df2bdcdfb5c98d7575e4
James Harrison <james@fasttortoise.co.uk>
2025-11-27 15:44:45 +0000

    Updated to new API

--- Common/src/main/java/net/minecraftcapes/mixin/MixinPlayerEarsModel.java
+++ Common/src/main/java/net/minecraftcapes/mixin/MixinPlayerEarsModel.java
@@ -8,25 +8,28 @@ import net.minecraft.client.model.geom.builders.CubeDeformation;
 import net.minecraft.client.model.geom.builders.CubeListBuilder;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerModel.class)
-public abstract class MixinPlayerModel extends HumanoidModel {
+public abstract class MixinPlayerEarsModel<T extends LivingEntity> extends HumanoidModel<T> {
 
-    public MixinPlayerModel(ModelPart root) {
-        super(root);
+    public MixinPlayerEarsModel(ModelPart param0) {
+        super(param0);
     }
-
-    @Inject(method = "createMesh", at = @At("RETURN"), cancellable = true)
-    private static void createMesh(CubeDeformation cubeDeformation, boolean slim, CallbackInfoReturnable<MeshDefinition> cir) {
+    
+    @Inject(method = "createMesh", at = @At(value = "RETURN"), cancellable = true)
+    private static void createEarsLayer(CubeDeformation param0, boolean param1, CallbackInfoReturnable<MeshDefinition> cir) {
         MeshDefinition meshDefinition = cir.getReturnValue();
         meshDefinition.getRoot().addOrReplaceChild("ear", CubeListBuilder.create(), PartPose.ZERO);
+        
         PartDefinition partDefinition = meshDefinition.getRoot().getChild("ear");
-        partDefinition.addOrReplaceChild("left_ear", CubeListBuilder.create().texOffs(0, 0).addBox(1.5F, -10.5F, -1.0F, 6, 6, 1, cubeDeformation, 0.21875F, 0.109375F), PartPose.ZERO);
-        partDefinition.addOrReplaceChild("right_ear", CubeListBuilder.create().texOffs(0, 0).addBox(-7.5F, -10.5F, -1.0F, 6, 6, 1, cubeDeformation, 0.21875F, 0.109375F), PartPose.ZERO);
+        partDefinition.addOrReplaceChild("left_ear", CubeListBuilder.create().texOffs(0, 0).addBox(1.5F, -10.5F, -1.0F, 6, 6, 1, param0, 0.21875F, 0.109375F), PartPose.ZERO);
+        partDefinition.addOrReplaceChild("right_ear", CubeListBuilder.create().texOffs(0, 0).addBox(-7.5F, -10.5F, -1.0F, 6, 6, 1, param0, 0.21875F, 0.109375F), PartPose.ZERO);
         cir.setReturnValue(meshDefinition);
     }
+
 }
---
+++ NeoForge/src/main/java/net/minecraftcapes/neoforge/events/RegisterKeyEvent.java
@@ -0,0 +1,20 @@
+package net.minecraftcapes.neoforge.events;
+
+import net.minecraftcapes.MinecraftCapes;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+
+@EventBusSubscriber(modid = MinecraftCapes.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class RegisterKeyEvent {
+    
+    /**
+     * Register the keybinds
+     * @param event
+     */
+    @SubscribeEvent
+    public static void registerKeyBinding(RegisterKeyMappingsEvent event) {
+        event.register(MinecraftCapes.KEY_MAPPING);
+    }
+}
