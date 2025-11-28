29e558864a2ae9f8fc4c5c5bc0ac5acad7183bfc
James Harrison <james@fasttortoise.co.uk>
2025-11-28 23:05:45 +0000

    Fix up GUI and add lang to keybind

--- src/main/java/net/minecraftcapes/gui/MenuScreen.java
+++ src/main/java/net/minecraftcapes/gui/MenuScreen.java
@@ -1,12 +1,17 @@
 package net.minecraftcapes.gui;
 
+import com.mojang.blaze3d.platform.GLX;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.platform.Lighting;
+import net.minecraft.Util;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.ConfirmLinkScreen;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.resources.language.I18n;
-import net.minecraft.network.chat.BaseComponent;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraftcapes.config.MinecraftCapesConfig;
 import net.minecraftcapes.player.DownloadManager;
 
@@ -23,17 +28,34 @@ public class MenuScreen extends Screen {
 
     @Override
     public void init() {
+        int xOffset = this.width / 6 * 4;
+        int yOffset = this.height / 3;
         int i = 0;
 
+        //Open MinecraftCapes
+        this.addButton(new Button(xOffset - 75, yOffset, 150, 20, "Open MinecraftCapes", (button) -> {
+            this.minecraft.setScreen(new ConfirmLinkScreen((openUrl) -> {
+                if(openUrl) {
+                    Util.getPlatform().openUri("https://minecraftcapes.net");
+                }
+
+                this.minecraft.setScreen(this);
+            }, "https://minecraftcapes.net", true));
+        }));
+        i++;
+
+        //For custom cape/ears onto new line
+        i++;
+
         //Custom Capes
-        this.addButton(new Button(this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, getMessage(GuiOption.CAPE), (button) -> {
+        this.addButton(new Button(xOffset - 155 + i % 2 * 160, yOffset + 24 * (i >> 1), 150, 20, getMessage(GuiOption.CAPE), (button) -> {
             MinecraftCapesConfig.setCapeVisible(!MinecraftCapesConfig.isCapeVisible());
             button.setMessage(getMessage(GuiOption.CAPE));
         }));
         i++;
 
         //Custom Ears
-        this.addButton(new Button(this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, getMessage(GuiOption.EARS), (button -> {
+        this.addButton(new Button(xOffset - 155 + i % 2 * 160, yOffset + 24 * (i >> 1), 150, 20, getMessage(GuiOption.EARS), (button -> {
             MinecraftCapesConfig.setEarsVisible(!MinecraftCapesConfig.isEarsVisible());
             button.setMessage(getMessage(GuiOption.EARS));
         })));
@@ -43,13 +65,13 @@ public class MenuScreen extends Screen {
         i++;
 
         //Reload Profile
-        this.addButton(new Button(this.width / 2 - 75, this.height / 6 + 24 * (i >> 1), 150, 20, "Reload Profile", (button -> {
+        this.addButton(new Button(xOffset - 75, yOffset + 24 * (i >> 1), 150, 20, "Reload Profile", (button -> {
             DownloadManager.prepareDownload(this.minecraft.player.getUUID(), this.minecraft.player.getName().getString(), true);
         })));
         i++;
 
         //Done
-        this.addButton(new Button(this.width / 2 - 100, this.height / 6 + 24 * (i >> 1), 200, 20, new TranslatableComponent("gui.done").getContents(), (button) -> {
+        this.addButton(new Button(xOffset - 100, yOffset + 24 * (i >> 1), 200, 20, I18n.get("gui.done"), (button) -> {
             this.minecraft.setScreen(null);
         }));
     }
@@ -57,8 +79,52 @@ public class MenuScreen extends Screen {
     @Override
     public void render(int mouseX, int mouseY, float partialTicks) {
         this.renderBackground();
-        this.drawCenteredString(this.font, this.title.getContents(), this.width / 2, 20, 16777215);
+        drawCenteredString(this.font, this.title.getContents(), this.width / 2, 20, 16777215);
+
+        renderPlayer(
+                this.width / 6,
+                this.height / 3 + 90,
+                60,
+                this.minecraft.player
+        );
+
         super.render(mouseX, mouseY, partialTicks);
+    }
+
+    private void renderPlayer(int x, int y, int scale, LivingEntity livingEntity) {
+        GlStateManager.enableColorMaterial();
+        GlStateManager.pushMatrix();
+        GlStateManager.translatef(x, y, 50.0F);
+        GlStateManager.scalef(scale, scale, scale);
+        GlStateManager.rotatef(180.0F, 1.0F, 0.0F, 0.0F);
+        float yBodyRot = livingEntity.yBodyRot;
+        float yRot = livingEntity.yRot;
+        float xRot = livingEntity.xRot;
+        float yHeadRotO = livingEntity.yHeadRotO;
+        float yHeadRot = livingEntity.yHeadRot;
+        Lighting.turnOn();
+        livingEntity.yBodyRot = 0.0F;
+        livingEntity.yRot = 0.0F;
+        livingEntity.xRot = 0.0F;
+        livingEntity.yHeadRot = livingEntity.yRot;
+        livingEntity.yHeadRotO = livingEntity.yRot;
+        GlStateManager.translatef(0.0F, 0.0F, 0.0F);
+        EntityRenderDispatcher entityRenderDispatcher = Minecraft.getInstance().getEntityRenderDispatcher();
+        entityRenderDispatcher.setPlayerRotY(180.0F);
+        entityRenderDispatcher.setRenderShadow(false);
+        entityRenderDispatcher.render(livingEntity, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, false);
+        entityRenderDispatcher.setRenderShadow(true);
+        livingEntity.yBodyRot = yBodyRot;
+        livingEntity.yRot = yRot;
+        livingEntity.xRot = xRot;
+        livingEntity.yHeadRotO = yHeadRotO;
+        livingEntity.yHeadRot = yHeadRot;
+        GlStateManager.popMatrix();
+        Lighting.turnOff();
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.activeTexture(GLX.GL_TEXTURE1);
+        GlStateManager.disableTexture();
+        GlStateManager.activeTexture(GLX.GL_TEXTURE0);
     }
 
     private String getMessage(GuiOption option) {
