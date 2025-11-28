acb7dcf06126e43b3acb4b4f9bcb6fc106f5522a
James Harrison <james@fasttortoise.co.uk>
2025-11-28 23:05:20 +0000

    Fix up GUI and add lang to keybind

--- common/src/main/java/net/minecraftcapes/gui/MenuScreen.java
+++ common/src/main/java/net/minecraftcapes/gui/MenuScreen.java
@@ -35,15 +35,9 @@ public class MenuScreen extends Screen {
         int i = 0;
         
         //Open MinecraftCapes
-        this.addRenderableWidget(Button.builder(Component.nullToEmpty("Open MinecraftCapes"), (button) -> {
-            this.minecraft.setScreen(new ConfirmLinkScreen((openUrl) -> {
-                if(openUrl) {
-                    Util.getPlatform().openUri("https://minecraftcapes.net");
-                }
-                
-                this.minecraft.setScreen(this);
-            }, "https://minecraftcapes.net", true));
-        }).bounds(xOffset - 75, yOffset, 150, 20).build());
+        this.addRenderableWidget(Button.builder(Component.nullToEmpty("Open MinecraftCapes"),
+            ConfirmLinkScreen.confirmLink("https://minecraftcapes.net", this, true)
+        ).bounds(xOffset - 75, yOffset, 150, 20).build());
         i++;
         
         //For custom cape/ears onto new line
