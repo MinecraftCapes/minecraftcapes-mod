cf6402025e96ea90fdb54bdcb1da98e489961270
James Harrison <james@fasttortoise.co.uk>
2025-11-29 13:12:41 +0000

    Cleanup GUI, fix crashes and add lang

--- src/main/java/net/minecraftcapes/mixin/MixinAbstractClientPlayer.java
+++ src/main/java/net/minecraftcapes/mixin/MixinAbstractClientPlayer.java
@@ -22,12 +22,12 @@ public abstract class MixinAbstractClientPlayer {
     protected abstract NetworkPlayerInfo getPlayerInfo();
 
     @Inject(method = "<init>", at = @At(value = "RETURN"))
-    private void downloadCape(World worldIn, GameProfile playerProfile, CallbackInfo ci) {
+    private void minecraftcapes$downloadPlayerInfo(World worldIn, GameProfile playerProfile, CallbackInfo ci) {
         DownloadManager.prepareDownload(playerProfile.getId(), playerProfile.getName(), false);
     }
 
     @Inject(method = "hasPlayerInfo", at = @At(value = "HEAD"), cancellable = true)
-    private void hasPlayerInfo(CallbackInfoReturnable<Boolean> cir) {
+    private void minecraftcapes$updatePlayerInfo(CallbackInfoReturnable<Boolean> cir) {
         PlayerHandler playerHandler = PlayerHandler.get(((AbstractClientPlayer) (Object) this).getUniqueID());
         if(playerHandler.getHasInfo() || this.getPlayerInfo() != null) {
             cir.setReturnValue(true);
@@ -35,7 +35,7 @@ public abstract class MixinAbstractClientPlayer {
     }
 
     @Inject(method = "getLocationCape", at = @At(value = "RETURN"), cancellable = true)
-    private void getLocationCape(CallbackInfoReturnable<ResourceLocation> cir) {
+    private void minecraftcapes$getLocationCape(CallbackInfoReturnable<ResourceLocation> cir) {
         NetworkPlayerInfo networkplayerinfo = this.getPlayerInfo();
         PlayerHandler playerHandler = PlayerHandler.get(((AbstractClientPlayer) (Object) this).getUniqueID());
         if(playerHandler.getCapeLocation() != null && MinecraftCapesConfig.isCapeVisible()) {
--- src/main/java/net/minecraftcapes/player/DownloadManager.java
+++ src/main/java/net/minecraftcapes/player/DownloadManager.java
@@ -67,7 +67,11 @@ public class DownloadManager {
                 BufferedImage capeImage = downloadOrLoad(profileResult.cape_url, "capes");
                 if (capeImage != null) {
                     playerHandler.applyCape(capeImage);
+                } else {
+                    playerHandler.removeCape();
                 }
+            } else {
+                playerHandler.removeCape();
             }
 
             // Download ears image if available
@@ -75,7 +79,11 @@ public class DownloadManager {
                 BufferedImage earsImage = downloadOrLoad(profileResult.ear_url, "ears");
                 if (earsImage != null) {
                     playerHandler.applyEars(earsImage);
+                } else {
+                    playerHandler.removeEars();
                 }
+            } else {
+                playerHandler.removeEars();
             }
         });
 
