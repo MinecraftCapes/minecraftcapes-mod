a4cfa39b74964e6746bdd4a6c76d04e491c6e21e
James Harrison <james@fasttortoise.co.uk>
2025-11-29 13:12:20 +0000

    Cleanup GUI, fix crashes and add lang

--- common/src/main/java/net/minecraftcapes/mixin/MixinDeadmau5EarsLayer.java
+++ common/src/main/java/net/minecraftcapes/mixin/MixinDeadmau5EarsLayer.java
@@ -21,24 +21,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Deadmau5EarsLayer.class)
 public abstract class MixinDeadmau5EarsLayer extends RenderLayer<PlayerRenderState, PlayerModel> {
-    
+
     @Shadow
     @Final
     private HumanoidModel<PlayerRenderState> model;
-    
+
     public MixinDeadmau5EarsLayer(RenderLayerParent<PlayerRenderState, PlayerModel> p_117346_) {
         super(p_117346_);
     }
-    
+
     @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/state/PlayerRenderState;FF)V", at = @At("HEAD"), cancellable = true)
-    public void render(PoseStack poseStack, MultiBufferSource p_116874_, int p_116875_, PlayerRenderState playerRenderState, float p_116877_, float p_116878_, CallbackInfo ci) {
+    public void minecraftcapes$renderEars(PoseStack poseStack, MultiBufferSource p_116874_, int p_116875_, PlayerRenderState playerRenderState, float p_116877_, float p_116878_, CallbackInfo ci) {
         //Cancel default render
         if(!playerRenderState.name.equalsIgnoreCase("deadmau5")) {
             ci.cancel();
         }
-        
+
         ExtendedRenderState extendedRenderState = (ExtendedRenderState) playerRenderState;
-        
+
         if (extendedRenderState.minecraftcapes$getEarsTexture() != null && !playerRenderState.isInvisible && extendedRenderState.minecraftcapes$getEarsEnabled()) {
             VertexConsumer vertexconsumer = p_116874_.getBuffer(RenderType.entitySolid(extendedRenderState.minecraftcapes$getEarsTexture()));
             int i = LivingEntityRenderer.getOverlayCoords(playerRenderState, 0.0F);
