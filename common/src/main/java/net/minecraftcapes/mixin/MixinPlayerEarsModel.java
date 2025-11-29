ca8b2c66734a68687dbd678cbb1b1452f81c4ccd
James Harrison <james@fasttortoise.co.uk>
2025-11-29 13:11:55 +0000

    Cleanup GUI, fix crashes and add lang

--- common/src/main/java/net/minecraftcapes/mixin/MixinPlayerEarsModel.java
+++ common/src/main/java/net/minecraftcapes/mixin/MixinPlayerEarsModel.java
@@ -20,12 +20,12 @@ public abstract class MixinPlayerEarsModel<T extends LivingEntity> extends Human
     public MixinPlayerEarsModel(ModelPart param0) {
         super(param0);
     }
-    
+
     @Inject(method = "createMesh", at = @At(value = "RETURN"), cancellable = true)
-    private static void createEarsLayer(CubeDeformation param0, boolean param1, CallbackInfoReturnable<MeshDefinition> cir) {
+    private static void minecraftcapes$createEars(CubeDeformation param0, boolean param1, CallbackInfoReturnable<MeshDefinition> cir) {
         MeshDefinition meshDefinition = cir.getReturnValue();
         meshDefinition.getRoot().addOrReplaceChild("ear", CubeListBuilder.create(), PartPose.ZERO);
-        
+
         PartDefinition partDefinition = meshDefinition.getRoot().getChild("ear");
         partDefinition.addOrReplaceChild("left_ear", CubeListBuilder.create().texOffs(0, 0).addBox(1.5F, -10.5F, -1.0F, 6, 6, 1, param0, 0.21875F, 0.109375F), PartPose.ZERO);
         partDefinition.addOrReplaceChild("right_ear", CubeListBuilder.create().texOffs(0, 0).addBox(-7.5F, -10.5F, -1.0F, 6, 6, 1, param0, 0.21875F, 0.109375F), PartPose.ZERO);
