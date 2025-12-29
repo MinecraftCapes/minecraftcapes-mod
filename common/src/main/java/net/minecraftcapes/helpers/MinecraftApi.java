f881e97cdb2b030022d580f47f450fea0a282ed3
James Harrison <james@fasttortoise.co.uk>
2025-12-29 17:34:21 +0000

    Work on 26.1

--- common/src/main/java/net/minecraftcapes/helpers/MinecraftApi.java
+++ common/src/main/java/net/minecraftcapes/helpers/MinecraftApi.java
@@ -44,7 +44,7 @@ public class MinecraftApi {
      */
     private static JsonObject getApiData(String data) {
         try {
-            URI uri = URI.create(String.format("https://api.minecraftapi.net/v3/profile/%s?params=[name,full_uuid]", data));
+            URI uri = URI.create(String.format("https://api.minecraftapi.net/v3/profile/%s?params=[full_uuid,name]", data));
             HttpURLConnection httpurlconnection = (HttpURLConnection) uri.toURL().openConnection(Minecraft.getInstance().getProxy());
             httpurlconnection.setRequestProperty("User-Agent", "minecraftcapes-mod/" + MINECRAFT_VERSION);
             httpurlconnection.setDoInput(true);
