2512538685f4579d568590c52ad0a107c0742ca3
James Harrison <james@fasttortoise.co.uk>
2026-01-04 18:18:51 +0000

    Update some new names

--- fabric/src/main/java/net/minecraftcapes/fabric/FabricClient.java
+++ fabric/src/main/java/net/minecraftcapes/fabric/FabricClient.java
@@ -2,7 +2,7 @@ package net.minecraftcapes.fabric;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.fabricmc.loader.impl.FabricLoaderImpl;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
@@ -17,7 +17,7 @@ public class FabricClient extends MinecraftCapes implements ClientModInitializer
         MinecraftCapes.onEnable(FabricLoaderImpl.INSTANCE.getConfigDir());
 
 		//Configure the KeyBind
-		keyBinding = KeyBindingHelper.registerKeyBinding(MinecraftCapes.KEY_MAPPING);
+		keyBinding = KeyMappingHelper.registerKeyMapping(MinecraftCapes.KEY_MAPPING);
 
 		//React to key pressed
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
