dadb247020f185f1a00816a6d5c1599b18e578ab
James Harrison <james@fasttortoise.co.uk>
2026-03-03 14:39:06 +0000

    Update license and API

--- src/main/java/net/minecraftcapes/helpers/MinecraftApi.java
+++ src/main/java/net/minecraftcapes/helpers/MinecraftApi.java
@@ -44,7 +44,7 @@ public class MinecraftApi {
      */
     private static JsonObject getApiData(String data) {
         try {
-            URI uri = URI.create("https://api.minecraftapi.net/api/v2/profile/" + data);
+            URI uri = URI.create("https://api.minecraftapi.net/v3/profile/" + data + "?params=[full_uuid,name]");
             HttpURLConnection httpurlconnection = (HttpURLConnection) uri.toURL().openConnection(Minecraft.getMinecraft().getProxy());
             httpurlconnection.setRequestProperty("User-Agent", "minecraftcapes-mod/" + MINECRAFT_VERSION);
             httpurlconnection.setDoInput(true);
