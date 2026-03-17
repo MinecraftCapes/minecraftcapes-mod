bacf496f7d2f3f8336792ef46b0fa4020cced239
James Harrison <james@fasttortoise.co.uk>
2026-03-17 01:40:44 +0000

    Work on Lunar support

---
+++ common/src/main/java/net/minecraftcapes/mixin/common/MixinCapeLayer.java
@@ -0,0 +1,40 @@
+package net.minecraftcapes.mixin.common;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.player.PlayerModel;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.CapeLayer;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.entity.state.AvatarRenderState;
+import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
+import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
+import net.minecraftcapes.player.ExtendedRenderState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(CapeLayer.class)
+public abstract class MixinCapeLayer extends RenderLayer<AvatarRenderState, PlayerModel> {
+
+    public MixinCapeLayer(RenderLayerParent<AvatarRenderState, PlayerModel> p_117346_) {
+        super(p_117346_);
+    }
+
+    @WrapOperation(method = "submit(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;ILnet/minecraft/client/renderer/entity/state/AvatarRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/SubmitNodeCollector;submitModel(Lnet/minecraft/client/model/Model;Ljava/lang/Object;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/rendertype/RenderType;IIILnet/minecraft/client/renderer/feature/ModelFeatureRenderer$CrumblingOverlay;)V"))
+    private void minecraftcapes$submitCape(SubmitNodeCollector instance, Model model, Object object, PoseStack poseStack, RenderType renderType, int i, int overlay, int outlineColor, ModelFeatureRenderer.CrumblingOverlay crumblingOverlay, Operation<Void> original, @Local AvatarRenderState avatarRenderState) {
+        ExtendedRenderState extendedRenderState = (ExtendedRenderState) avatarRenderState;
+        if(extendedRenderState.minecraftcapes$getCapeEnabled()) {
+            instance.order(0).submitModel(model, object, poseStack, RenderTypes.armorCutoutNoCull(avatarRenderState.skin.cape().texturePath()), i, overlay, -1, null, outlineColor, crumblingOverlay);
+            if(extendedRenderState.minecraftcapes$hasCapeGlint()) {
+                instance.order(1).submitModel(model, object, poseStack, RenderTypes.armorEntityGlint(), i, overlay, -1, null, outlineColor, crumblingOverlay);
+            }
+        } else {
+            original.call(instance, model, object, poseStack, renderType, i, overlay, outlineColor, crumblingOverlay);
+        }
+    }
+}
