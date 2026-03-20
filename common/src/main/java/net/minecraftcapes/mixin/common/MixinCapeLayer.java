e40cf19f1132188586fb9330826aa60f74fc18d8
James Harrison <james@fasttortoise.co.uk>
2026-03-20 19:04:15 +0000

    Start Lunar Support

--- common/src/main/java/net/minecraftcapes/mixin/common/MixinCapeLayer.java
+++ common/src/main/java/net/minecraftcapes/mixin/common/MixinCapeLayer.java
@@ -1,5 +1,7 @@
-package net.minecraftcapes.mixin;
+package net.minecraftcapes.mixin.common;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.PlayerModel;
@@ -13,22 +15,21 @@ import net.minecraft.client.renderer.entity.state.PlayerRenderState;
 import net.minecraftcapes.player.ExtendedRenderState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(CapeLayer.class)
+@Mixin(value = CapeLayer.class, priority = 0)
 public abstract class MixinCapeLayer extends RenderLayer<PlayerRenderState, PlayerModel> {
-    
+
     public MixinCapeLayer(RenderLayerParent<PlayerRenderState, PlayerModel> p_117346_) {
         super(p_117346_);
     }
 
-    @Redirect(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/state/PlayerRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/MultiBufferSource;getBuffer(Lnet/minecraft/client/renderer/RenderType;)Lcom/mojang/blaze3d/vertex/VertexConsumer;"))
-    public VertexConsumer minecraftcapes$renderGlint(MultiBufferSource instance, RenderType renderType, @Local PlayerRenderState playerRenderState) {
+    @WrapOperation(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/state/PlayerRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/MultiBufferSource;getBuffer(Lnet/minecraft/client/renderer/RenderType;)Lcom/mojang/blaze3d/vertex/VertexConsumer;"))
+    private VertexConsumer minecraftcapes$renderGlint(MultiBufferSource instance, RenderType renderType, Operation<VertexConsumer> original, @Local(argsOnly = true) PlayerRenderState playerRenderState) {
         ExtendedRenderState minecraftcapes$playerRenderState = (ExtendedRenderState) playerRenderState;
         if(minecraftcapes$playerRenderState.minecraftcapes$getCapeEnabled()) {
             return ItemRenderer.getArmorFoilBuffer(instance, RenderType.armorCutoutNoCull(playerRenderState.skin.capeTexture()), minecraftcapes$playerRenderState.minecraftcapes$hasCapeGlint());
         } else {
-            return instance.getBuffer(renderType);
+            return original.call(instance, renderType);
         }
     }
 }
